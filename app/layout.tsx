c004ff28af3d51a9b9d32b50704370b4796a965f
김선규 <>
2022-12-23 17:35:29 +0900

    update: update tailwind & some code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
+import React from "react";
+import "#/styles/globals.css";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
